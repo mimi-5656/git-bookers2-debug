d0a734d74e385c799e26acebe598de270e845b2d
EC2 Default User <ec2-user@ip-172-31-0-52.ap-northeast-1.compute.internal>
2022-10-04 12:45:28 +0000

    サインアップまで

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   belongs_to :books
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true, presence:true
+  validates :introduction, length: { maximum: 50 }
 
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
